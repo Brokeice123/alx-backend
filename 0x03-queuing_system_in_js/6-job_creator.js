8e041818e6c9ceb7d61b96a2558f1bde969f90e6
Brokeice123 <keithmarkomen@gmail.com>
2024-06-25 11:59:28 +0300

    Create the Job creator

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,25 @@
+import kue from 'kue';
+
+// Create a queue
+const queue = kue.createQueue();
+
+// Job data
+const jobData = {
+  phoneNumber: '1234567890',
+  message: 'This is the notification message'
+};
+
+// Create a job
+const job = queue.create('push_notification_code', jobData)
+  .save((err) => {
+    if (!err) {
+      console.log(`Notification job created: ${job.id}`);
+    }
+  });
+
+// Event listeners for the job
+job.on('complete', () => {
+  console.log('Notification job completed');
+}).on('failed', () => {
+  console.log('Notification job failed');
+});
